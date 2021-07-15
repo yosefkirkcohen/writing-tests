39c2c142d901fc9c049c04b157cd238c788ea905
yosefkirkcohen <ycohen45@yahoo.com>
2021-07-15 16:59:02 -0700

    tests worked

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,48 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { adder, subtracter, multiplier, divider} from '../utils.js'
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('adder should take in 4 and 2 and return 6', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 6;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = adder(4, 2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('subtracter should take in 8 and 3 and return 5', (expect) => {
+
+    const expected = 5;
+
+    const actual = subtracter(8, 3);
+
+    expect.equal(actual, expected);
+});
+
+test('multiplier should take 3 and 7 and return 21', (expect) => {
+
+    const expected = 21;
+
+    const actual = multiplier(3,7);
+
+    expect.equal(actual, expected);
+})
+
+test('divider should take 10 and 5 and return 2', (expect) => {
+
+    const expected = 2;
+
+    const actual = divider(10, 5);
+
+    expect.equal(actual, expected);
+})
+
---
+++ utils.js
@@ -0,0 +1,15 @@
+export function adder(num1,num2) {
+    return num1 + num2;
+}
+
+export function subtracter (num1,num2) {
+    return num1 - num2;
+}
+
+export function multiplier (num1,num2) {
+    return num1 * num2;
+}
+
+export function divider (num1,num2) {
+    return num1 / num2;
+}
